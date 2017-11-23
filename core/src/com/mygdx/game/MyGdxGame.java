63cc0829cc67ef49d4bd4c355fea48a16ca6c0fb
Roman Verteletsky <verteletsky@bitrix24.ru>
2017-11-23 23:22:43 +0300

    init

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,47 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+    private Texture img;
+
+    @Override
+    public void create() {
+        img = new Texture(Gdx.files.internal("Logo.png"), true);
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+}
